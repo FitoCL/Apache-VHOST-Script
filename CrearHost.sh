9250a710d0a8897418e1df3e5ee0506515b800ea
Fito <152565221+FitoCL@users.noreply.github.com>
2025-02-11 04:05:02 -0300

    Update CrearHost.sh

--- CrearHost.sh
+++ CrearHost.sh
@@ -2,16 +2,16 @@
 # Author: Fito
 #http://www.facebook.com/fitog2 copyleft 2011
 ######################################################## INTRUCCIONES #############################################################
-#abre una consola y nos movemos ¡Recuerda como Super Usuario root!																  #																									                              #
-#Para ejecutar el script seguir el ejemplo:																						  #
-#																																  #
-#     sh crearhost midominio ipaddress /carpeta/proyecto 
-#	  Ejemplo: 	crearhost cursosdeservers.org 192.168.1.128 /$HOME/Paginas/proyecto1 																      #
-#																																  #
+#abre una consola y nos movemos ¡Recuerda como Super Usuario root!					                          #																									                              #
+#Para ejecutar el script seguir el ejemplo:											  #
+#																  #
+#     sh crearhost midominio ipaddress /carpeta/proyecto                                                                          #
+#	  Ejemplo: 	crearhost cursosdeservers.org 192.168.1.128 /$HOME/Paginas/proyecto1 					  #
+#															          #															 
 #Con eso el script configurará el virtual host con la ip de la máquina, el archivo hosts, así como el dominio local.              #
-#1- El segundo parametro es opcional, si no envías un segundo parametro, tomará la ip actual de la máquina. 					  #
-#2 -El tercer parametro es opcional, si no envías un tercer parametro te 														  #
-#creará el proyecto en la carpeta default de Apache: “/var/www/”.																  #
+#1- El segundo parametro es opcional, si no envías un segundo parametro, tomará la ip actual de la máquina. 			  #	  
+#2 -El tercer parametro es opcional, si no envías un tercer parametro te 							  #							  
+#creará el proyecto en la carpeta default de Apache: “/var/www/”.								  #								  
 ###################################################################################################################################
 #Comprobamos que el usuario es root.
 if [ $(whoami) != "root" ]; then
